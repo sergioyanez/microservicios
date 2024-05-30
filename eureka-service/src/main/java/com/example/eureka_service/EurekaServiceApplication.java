da3e1e4b01a31b6b1cc8265eaa4f9b44cde3ef46
Sergio Yañez <sergiomyanez01@gmail.com>
2024-05-29 23:16:27 -0300

    Se implementa servidor Eureka

--- eureka-service/src/main/java/com/example/eureka_service/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/example/eureka_service/EurekaServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.eureka_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
--- microservicio-user/src/main/java/com/example/microserviciouser/MicroservicioUserApplication.java
+++ microservicio-user/src/main/java/com/example/microserviciouser/MicroservicioUserApplication.java
@@ -2,10 +2,12 @@ package com.example.microserviciouser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class MicroservicioUserApplication {
 
 	public static void main(String[] args) {
