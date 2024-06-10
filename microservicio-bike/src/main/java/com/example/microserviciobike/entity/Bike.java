db5f5fa99666427b3d13086aa2ec853b9e5449d1
sergioyanez <sergiomyanez01@gmail.com>
2024-06-10 01:16:53 -0300

    mongodb

--- microservicio-bike/src/main/java/com/example/microserviciobike/entity/Bike.java
+++ microservicio-bike/src/main/java/com/example/microserviciobike/entity/Bike.java
@@ -1,20 +1,22 @@
 package com.example.microserviciobike.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
+//@Entity
+@Document(collection = "bikes")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Bike {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String brand;
     private String  model;
--- microservicio-bike/src/main/java/com/example/microserviciobike/repository/BikeRepository.java
+++ microservicio-bike/src/main/java/com/example/microserviciobike/repository/BikeRepository.java
@@ -1,13 +1,15 @@
 package com.example.microserviciobike.repository;
 
 import com.example.microserviciobike.entity.Bike;
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BikeRepository extends JpaRepository<Bike,Long>{
+//public interface BikeRepository extends JpaRepository<Bike,Long>{
+public interface BikeRepository extends MongoRepository<Bike,Long> {
 
     List<Bike> findByUserId(Long userId);
 }
